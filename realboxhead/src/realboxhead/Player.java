c842de8610440353c4fbc350f94847c31c9e3761
298225001 <298225001@W865-1533.edu.cbe.ab.ca>
2015-10-27 13:13:52 -0600

    baux

---
+++ realboxhead/src/realboxhead/Player.java
@@ -0,0 +1,31 @@
+package realboxhead;
+
+public class Player {
+
+	int x;
+	int y;
+	int health;
+	int speed;
+	int width;
+	int height;
+	boolean isCollide;
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+	public int getHP() {
+		return health;
+	}
+	
+	public int getSpd() {
+		return speed;
+	}
+	
+	
+
+}
