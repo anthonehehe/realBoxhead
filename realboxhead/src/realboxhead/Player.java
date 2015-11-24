39692738c2362c9a0425d9ddb9e1a6705884780d
298225001 <298225001@W865-1533.edu.cbe.ab.ca>
2015-11-24 13:56:16 -0700

    gftrdl

--- realboxhead/src/realboxhead/Player.java
+++ realboxhead/src/realboxhead/Player.java
@@ -1,31 +1,88 @@
 package realboxhead;
 
-public class Player {
+import java.awt.Graphics;
 
-	int x;
-	int y;
+public class Player extends GameObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	int health;
-	int speed;
-	int width;
-	int height;
-	boolean isCollide;
+	int speed = 4;
+	public boolean playerUp = false;
+	public boolean playerDown;
+	public boolean playerLeft;
+	public boolean playerRight;
+	String name;
 
-	public int getX() {
-		return x;
+	public Player(String name) {
+		x = 50;
+		y = 50;
+		height = 30;
+		width = 15;
+		this.name = name;
 	}
 
-	public int getY() {
-		return y;
+	public void draw(Graphics g) {
+		g.drawRect(x, y, height, width);
+		// g.fillRect(x,y,height,width);
 	}
-	
+
 	public int getHP() {
 		return health;
 	}
-	
+
+	public int setSpd() {
+		// equal to 100 (default speed) - weight of the gun
+	}
+
 	public int getSpd() {
 		return speed;
 	}
-	
-	
+
+	// public boolean checkCollide() {
+	// }
+
+	public void moveLeft() {
+
+		x -= getSpd();
+		System.out.println("left");
+
+	}
+
+	public void moveRight() {
+
+		x += getSpd();
+
+	}
+
+	public void moveUp() {
+
+		y -= getSpd();
+
+	}
+
+	public void moveDown() {
+
+		y += getSpd();
+
+	}
+
+	public void movement() {
+		if (playerUp) {
+			moveUp();
+		}
+		if (playerDown) {
+			moveDown();
+		}
+		if (playerLeft) {
+			moveLeft();
+		}
+		if (playerRight) {
+			moveRight();
+		}
+
+	}
 
 }
